463d2cc0321e7177d68d08b5bedd24a4046b41b5
Jon Earley <jonathan.a.earley@gmail.com>
2018-06-15 09:45:34 -0400

    Added more knob options to Expandable

--- src/components/Expandable/Expandable-story.js
+++ src/components/Expandable/Expandable-story.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-import { text, boolean, select } from '@storybook/addon-knobs';
+import { text, number, boolean, select } from '@storybook/addon-knobs';
 
 import Expandable, { ExpandableChildren, ExpandableButton } from '../Expandable';
 
@@ -31,12 +31,18 @@ storiesOf('Expandable', module)
     `)(() =>
       <Expandable>
         <ul>
-          <ExpandableChildren show={4}>
+          <ExpandableChildren
+            show={number('Show initially', 3)}
+          >
             {items.map((item, key) => <li key={key}>{item}</li>)}
           </ExpandableChildren>
         </ul>
 
-        <ExpandableButton kind="tertiary" />
+        <ExpandableButton
+          kind="tertiary"
+          name={text('Show all name', 'colors')}
+          count={number('Show all count', items.length)}
+        />
       </Expandable>
     )
   )
