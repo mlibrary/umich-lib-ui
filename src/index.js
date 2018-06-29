c6c1d633d6f93c2aed49423409597076135f66f4
Jon Earley <jonathan.a.earley@gmail.com>
2018-06-29 16:04:33 -0400

    feat(chore): index setup for accessing components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './globals/scss/styles.css'
-
-const App = () => (
-  <p>University of Michigan Library components built in React</p>
-)
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+export Button from './components/Button'
+export Expandable, {
+  ExpandableButton,
+  ExpandableChildren
+} from './components/Expandable'
+export Header from './components/Header'
+export RecordAccess from './components/RecordAccess'
+export RecordFields from './components/RecordFields'
