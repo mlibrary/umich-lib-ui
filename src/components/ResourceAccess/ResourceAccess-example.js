12e48486244813be495e773a8a26ab8efccf7439
Jon Earley <jonathan.a.earley@gmail.com>
2018-08-14 10:41:51 -0400

    Updated resourceAccess example to include icon states.

--- src/components/ResourceAccess/ResourceAccess-example.js
+++ src/components/ResourceAccess/ResourceAccess-example.js
@@ -1,24 +1,29 @@
 <ResourceAccess
-  caption={"HathiTrust Digital Library"}
-  captionLink={{ text: 'About location', href: '#' }}
-  notes={['Library has: 1-7 : 1979-1986']}
-  headings={["Link", "Description", "Source"]}
+  caption={"Research Museums Center Birds Division"}
+  captionLink={{ text: 'About location', href: 'http://www.lib.umich.edu/location/museums-library/unit/31' }}
+  notes={['Library has: 1- : 2012-']}
+  headings={["Link", "Description", "Status"]}
   rows={[
     [
-      { text: "Search only (no full text)", href: "http://hdl.handle.net/2027/mdp.39015055430386" },
-      { text: "no.1-3 1944-1945" },
-      { text: "University of Michigan" }
+      { text: "Get this", href: "https://search.lib.umich.edu/catalog/record/012977832/get-this/39015072357000" },
+      { text: "v.1 2012" },
+      { text: "On shelf", intent: 'success', icon: 'check-circle' }
     ],
     [
-      { text: "Get this", to: "http://hdl.handle.net/2027/mdp.39015055430394" },
-      { text: "no.4-6 19uu" },
-      { text: "University of Michigan" }
-    ]
+      { text: "Get this", href: "https://search.lib.umich.edu/catalog/record/012977832/get-this/39015072357000" },
+      { text: "v.2 2013" },
+      { text: "Missing", intent: 'warning', icon: 'warning' }
+    ],
+    [
+      { text: "Get this", href: "https://search.lib.umich.edu/catalog/record/012977832/get-this/39015072357000" },
+      { text: "v.2 2014" },
+      { text: "Checked out", intent: 'error', icon: 'error' }
+    ],
   ]}
   renderAnchor={data => (
     <a href={data.to}>
       {data.text}
     </a>
   )}
-  name={"HathiTrust Sources"}
+  name={"Holdings"}
 />
