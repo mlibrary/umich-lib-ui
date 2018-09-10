25dc005dbeffe3f3c62b247208c296f6f02f23b7
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-10 15:49:34 -0400

    experimenting with emotion and building packages

---
+++ packages/styles/src/index.js
@@ -0,0 +1,68 @@
+/*
+  Brand colors
+*/
+
+export const color__um_yellow = '#FFCB05'
+export const color__um_blue = '#00274C'
+
+/*
+  Greys
+*/
+
+export const color__grey_1 = '#262626'
+export const color__grey_2 = '#4E4E4E'
+export const color__grey_3 = '#CCC'
+
+export const color__grey_light_1 = '#E5E5E5'
+export const color__grey_light_2 = '#F2F2F2'
+export const color__grey_light_3 = '#FAFAFA'
+
+/*
+  Blues
+*/
+
+export const color__blue_1 = color__um_blue
+export const color__blue_2 = '#0C5292'
+export const color__blue_3 = '#126DC1'
+
+export const color__blue_light_1 = '#CCE6FF'
+export const color__blue_light_2 = '#E6F3FF'
+export const color__blue_light_3 = '#F2F9FF'
+
+/*
+  Greens
+*/
+
+export const color__green_1 = '#057C42'
+export const color__green_2 = '#05A657'
+export const color__green_3 = '#E2F4EB'
+
+/*
+  Oranges
+*/
+
+export const color__orange_1 = '#AA5600'
+export const color__orange_2 = '#E77504'
+export const color__orange_3 = '#FFEEDD'
+
+/*
+  Reds
+*/
+
+export const color__red_1 = '#C53B26'
+export const color__red_2 = '#ED5D47'
+export const color__red_3 = '#FFEAE7'
+
+/*
+  Teals
+*/
+
+export const color__teal_1 = '#AA5600'
+export const color__teal_2 = '#E77504'
+export const color__teal_3 = '#FFEEDD'
+
+/*
+  Purple
+*/
+
+export const color__purple_1 = '#4c2c92'
--- shared/build-package.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const execSync = require("child_process").execSync
-
-console.log("\nBuilding ES modules ...");
-execSync(`babel src -d es --presets=@babel/preset-react --plugins=transform-object-rest-spread,transform-class-properties`);
