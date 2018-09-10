7c4c3b9ebea24bf16420cdad9b708160363dd14f
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-10 17:44:01 -0400

    Updated button and base styles.

--- packages/button/examples/basic.example.js
+++ packages/button/examples/basic.example.js
@@ -4,5 +4,16 @@ import Button from "../src/index";
 export let name = "primary";
 
 export let Example = () => (
-  <Button>Basic</Button> 
+  <ul className="y-spacing">
+    <li className="x-spacing">
+      <Button>Continue</Button>
+      <Button small>Continue</Button>
+    </li>
+    <li className="x-spacing">
+      <Button kind="secondary">Continue</Button>
+      <Button kind="secondary" small>Continue</Button></li>
+    <li className="x-spacing">
+      <Button kind="tertiary">Continue</Button>
+      <Button kind="tertiary" small>Continue</Button></li>
+  </ul>
 )
