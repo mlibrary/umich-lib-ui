09728b8ec7e877272c65ca1aa5312643f00eda0a
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-11 10:24:03 -0400

    updated storybook expamples

---
+++ packages/button/examples/all.example.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../src/index";
+
+export let name = "All";
+
+export let Example = () => (
+  <ul className="y-spacing">
+    <li><Button>Primary</Button></li>
+    <li><Button kind="secondary">Secondary</Button></li>
+    <li><Button kind="tertiary">Tertiary</Button></li>
+    <li><Button small>Primary small</Button></li>
+    <li><Button kind="secondary" small>Secondary small</Button></li>
+    <li><Button kind="tertiary" small>Tertiary small</Button></li>
+  </ul>
+)
