e3edf453b1fd273d6065c70fb65b9d3bee32eb5a
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-11 21:50:46 -0400

    Alert added.

--- packages/chat/examples/fixed.example.js
+++ packages/chat/examples/fixed.example.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Chat from "../src/index";
 
-export let name = "fixed to viewport";
+export let name = "fixed to viewport on large screens";
 
 export let Example = () => (
   <Chat fixed />
