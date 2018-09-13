426c274657c9534f85902f50f11e896917b4fe36
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-13 19:18:22 -0400

    doc layout tweaks

--- www/src/components/sidenav.js
+++ www/src/components/sidenav.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Heading from '../../../packages/heading'
+import styled from 'react-emotion'
+import { css } from 'emotion'
+
+const StyledList = styled('ul')({
+  marginBottom: '2rem'
+})
+
+const cssActiveNavLink = css`
+  font-weight: 600;
+`
+
+const StyledNavLink = styled(Link)({
+  textDecoration: 'none'
+})
 
 const SideNav = ({ data }) => {
   return (
-    <nav className="docs__side-nav docs__side-nav--large-screens">
-      <ul>
+    <nav>
+      <StyledList>
         {data.map((section, s) => (
           <li key={s}>
             {section.to ? (
-              <Link to={section.to} activeClassName="side-nav-item--active">{section.title}</Link>
+              <StyledNavLink to={section.to} activeClassName={cssActiveNavLink}>{section.title}</StyledNavLink>
             ) : (
               <Heading size="small" level={2}>{section.title}</Heading>
             )}
             {section.items && (
               <ul>
                 {section.items.map((item, i) => (
-                  <li key={i}><Link to={item.to} activeClassName="side-nav-item--active">{item.title}</Link></li>
+                  <li key={i}><StyledNavLink to={item.to} activeClassName={cssActiveNavLink}>{item.title}</StyledNavLink></li>
                 ))}
               </ul>
             )}
           </li>
         ))}
-      </ul>
+      </StyledList>
     </nav>
   )
 }
