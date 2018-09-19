87b1e143af7f40b5b9a45264d509d28f547920e2
Jon Earley <jonathan.a.earley@gmail.com>
2018-09-19 09:36:48 -0400

    .

--- www/src/components/sidenav.js
+++ www/src/components/sidenav.js
@@ -1,43 +1,34 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Heading from '../../../packages/heading'
+import { colors, MEDIA_QUERIES } from '../../../packages/styles'
 import styled from 'react-emotion'
 import { css } from 'emotion'
 
-const StyledList = styled('ul')({
-  marginBottom: '2rem'
-})
-
-const cssActiveNavLink = css`
-  font-weight: 600;
-`
-
-const StyledNavLink = styled(Link)({
-  textDecoration: 'none'
+const SectionNav = styled('nav')({
+  gridArea: 'side'
 })
 
 const SideNav = ({ data }) => {
   return (
-    <nav>
-      <StyledList>
-        {data.map((section, s) => (
-          <li key={s}>
-            {section.to ? (
-              <StyledNavLink to={section.to} activeClassName={cssActiveNavLink}>{section.title}</StyledNavLink>
-            ) : (
+    <React.Fragment>
+      <SectionNav>
+        <ul>
+          {data.map((section, s) => (
+            <li key={s}>
               <Heading size="small" level={2}>{section.title}</Heading>
-            )}
-            {section.items && (
-              <ul>
-                {section.items.map((item, i) => (
-                  <li key={i}><StyledNavLink to={item.to} activeClassName={cssActiveNavLink}>{item.title}</StyledNavLink></li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </StyledList>
-    </nav>
+              {section.items && (
+                <ul>
+                  {section.items.map((item, i) => (
+                    <li key={i}><Link to={item.to}>{item.title}</Link></li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </SectionNav>
+    </React.Fragment>
   )
 }
 
