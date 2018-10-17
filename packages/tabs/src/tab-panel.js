29ab7e39b443f2512b04a5131a5b415b571acf17
Jon Earley <jonathan.a.earley@gmail.com>
2018-10-17 15:58:03 -0400

    Tab semantics with ARIA working. Unstyled.

---
+++ packages/tabs/src/tab-panel.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import TabContext from './tab-context'
+import { colors } from '@umich-lib-ui/styles'
+
+class TabPanel extends React.Component {
+  render() {
+    const {
+      children,
+      id,
+      selectedTab
+    } = this.props;
+    const hidden = !(selectedTab && selectedTab.slice(1) === id)
+
+    return (
+      <section
+        style={{
+          display: hidden ? "none" : "block"
+        }}
+        id={id}
+        tabIndex="0"
+        role="tabpanel"
+        aria-labeledby={`tab-to-#${id}`}
+        hidden={hidden}
+      >
+        {children}
+      </section>
+    );
+  }
+}
+
+export default props => (
+  <TabContext.Consumer>
+    {contextProps => (
+      <TabPanel
+        {...contextProps}
+        {...props}
+      />
+    )}
+  </TabContext.Consumer>
+)
---
+++ packages/tabs/src/tab.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+class Tab extends React.Component {
+  tabRef = React.createRef();
+
+  componentDidUpdate() {
+    if (this.props.selected) {
+      this.tabRef.current.focus()
+    }
+  }
+
+  render() {
+    const {
+      selected,
+      children,
+      to,
+      handleTabClick,
+      handleTabKeyDown
+    } = this.props
+
+    return (
+      <li
+        role="presentation"
+      >
+        <a
+          href={to}
+          role="tab"
+          tabIndex={selected ? '0' : '-1'}
+          aria-selected={selected}
+          onClick={(e) => {
+            e.preventDefault()
+            handleTabClick()
+          }}
+          id={`tab-to-${to}`}
+          ref={this.tabRef}
+          onKeyDown={(e) => {
+            handleTabKeyDown(e)
+          }}
+        >
+          {children}
+        </a>
+      </li>
+    )
+  }
+}
+
+export default Tab
