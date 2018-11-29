2012dcdb75b0b6c034ce165465b6d7dd0ea0bf05
Jon Earley <jonathan.a.earley@gmail.com>
2018-11-29 16:11:00 -0500

    Improved Button example

--- packages/button/examples/with-class.example.js
+++ packages/button/examples/with-class.example.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Button from "../src";
 
-export let name = "with class";
+export let name = "with class and style";
 
 export let Example = () => (
-  <Button kind="secondary" className="button" style={{ border: 'none' }}>Start</Button>
+  <Button kind="secondary" className="button" style={{ border: 'none' }}>Styled to be blue text</Button>
 )
