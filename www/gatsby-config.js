277079e8885f13d9a39c7cde77f40f36752927ae
Jon Earley <jonathan.a.earley@gmail.com>
2018-12-11 12:25:34 -0500

    .

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
