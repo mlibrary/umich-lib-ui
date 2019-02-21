92829a4815d2fd976dc2378554e715cbf3f33183
Jon Earley <jonathan.a.earley@gmail.com>
2019-02-20 20:02:53 -0500

    .

--- packages/alert/examples/informational.example.js
+++ packages/alert/examples/informational.example.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Alert from "../src/index";
 
-export let name = "info";
+export let name = "informational";
 
 export let Example = () => (
   <Alert intent="informational">An informational Alert.</Alert>
