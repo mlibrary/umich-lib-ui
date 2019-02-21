fc0e5e2f9a81a11ce218776fbdb05aa092de9c99
Jon Earley <jonathan.a.earley@gmail.com>
2019-02-21 10:44:47 -0500

    Adding more Alert stories.

--- packages/alert/examples/success.example.js
+++ packages/alert/examples/success.example.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Alert from "../src/index";
 
-export let name = "informational";
+export let name = "success";
 
 export let Example = () => (
-  <Alert intent="informational">Hello there.</Alert>
+  <Alert intent="success">You did it!</Alert>
 )
