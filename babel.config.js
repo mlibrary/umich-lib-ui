2e497a5cf23e7ec401b9617935d6b384a16ef341
Jon Earley <jonathan.a.earley@gmail.com>
2019-03-27 12:17:56 -0400

    Some setup for Jest.

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    "transform-class-properties",
+    "@babel/plugin-proposal-object-rest-spread"
+  ]
+};
