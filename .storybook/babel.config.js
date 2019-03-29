eb182a5f79f86feac6586611355c6a76901733f1
Jon Earley <jonathan.a.earley@gmail.com>
2019-03-29 12:38:20 -0400

    Storybook exports bugfix.

---
+++ .storybook/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  /*
+    For some reason this is required.
+    Others with same issue: https://github.com/storybooks/storybook/issues/3346#issuecomment-469631919
+  */
+  "sourceType": "unambiguous"
+}
