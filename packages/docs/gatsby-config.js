81dbf6b916999ab46f22fc3c31aadcaa5b7aa31c
Jon Earley <jonathan.a.earley@gmail.com>
2019-04-23 16:15:35 -0400

    Update theme color.

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -39,8 +39,8 @@ module.exports = {
         name: 'gatsby-default-mdx-basic',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#FFDA50',
+        theme_color: '#FFDA50',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
