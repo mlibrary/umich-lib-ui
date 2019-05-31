b024a9f0eac3d6cd9befbb73e5891ec6d3e9c8ec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-31 15:02:59 +0000

    Deploy mlibrary/umich-lib-ui to github.com/mlibrary/umich-lib-ui.git:gh-pages

--- component---src-pages-404-js-e88381233a46e298c819.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{262:function(e,a,n){"use strict";n.r(a);var t=n(4),i=(n(0),n(1)),o=n(316),c=n(315);a.default=function(){return Object(t.a)(o.a,null,Object(t.a)(c.a,{title:"404: Not found"}),Object(t.a)(i.Margins,null,Object(t.a)("div",{style:{margin:"4rem 0"},className:"y-spacing"},Object(t.a)(i.Heading,{level:1,size:"3XL"},"Page not found - 404"),Object(t.a)(i.Text,{size:"XS"},"What you're looking for isn't here."))))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e88381233a46e298c819.js.map
