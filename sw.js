84feff547e9bb30b1763b896c046fd6afb831322
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-01 14:22:31 +0000

    Deploy mlibrary/umich-lib-ui to github.com/mlibrary/umich-lib-ui.git:gh-pages

--- sw.js
+++ sw.js
@@ -26,20 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-56145500e23b42e37a1a.js"
+    "url": "webpack-runtime-593205b46837dd566dab.js"
   },
   {
     "url": "commons-eb09c07ab1d26ee74619.js"
   },
   {
-    "url": "app-ce6c3b76d366e6781a5a.js"
+    "url": "app-2cc6c837606468080767.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-f9d328f7b3c863df84fd.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7487712020323533aa58d77cb8e6bcd8"
+    "revision": "4dbd5653b590a5e6716216da729578f6"
   },
   {
     "url": "manifest.webmanifest",
@@ -67,7 +67,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-ce6c3b76d366e6781a5a.js`))) {
+  if (!resources || !(await caches.match(`/app-2cc6c837606468080767.js`))) {
     return await fetch(event.request)
   }
 
