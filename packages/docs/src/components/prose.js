1182a3257508768da678b861c25a2fd19e5cf222
Jon Earley <jonathan.a.earley@gmail.com>
2019-11-11 16:22:02 -0500

    Style direct children.

--- packages/docs/src/components/prose.js
+++ packages/docs/src/components/prose.js
@@ -9,16 +9,16 @@ const Prose = styled("div")({
   "> div > *": {
     marginBottom: SPACING["M"]
   },
-  "h2:not(:last-child)": {
+  "> h2:not(:last-child)": {
     marginTop: SPACING["2XL"]
   },
-  "h3:not(:last-child)": {
+  "> h3:not(:last-child)": {
     marginTop: SPACING["XL"]
   },
-  "h4:not(:last-child)": {
+  "> h4:not(:last-child)": {
     marginTop: SPACING["XL"]
   },
-  "h5:not(:last-child)": {
+  "> h5:not(:last-child)": {
     marginTop: SPACING["XL"]
   }
 });
